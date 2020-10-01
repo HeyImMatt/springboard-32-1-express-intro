b1da15eaf0f32ec35a36cf795965a887ccd7b6b8
heyimmatt <mrmcfarland@gmail.com>
2020-09-30 22:37:44 -0700

    Add mean get route

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/mean', function(req, res, next) {
+  try {
+    const operation = 'mean';
+    if (!req.query.nums) {
+      throw new ExpressError('Nums required', 400);
+    }
+    const nums = req.query.nums.split(',');
+    const value = nums.reduce((acc, n, i) => {
+      if (!parseInt(n)) {
+        throw new ExpressError(`${n} is not a number`, 400)
+      };
+      if (i === nums.length - 1) {
+        acc += parseInt(n);
+        return acc / nums.length;
+      }
+      return acc += parseInt(n);
+    }, 0)
+    return res.json({operation, value});
+  } catch(e) {
+    return next(e);
+  }
+});
+
 app.use(function(err, req, res, next) {
   // the default status is 500 Internal Server Error
   let status = err.status || 500;
