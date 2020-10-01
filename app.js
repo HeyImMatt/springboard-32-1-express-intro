47a73b9a53e676058aa114563b1193c8048a5185
heyimmatt <mrmcfarland@gmail.com>
2020-09-30 22:50:32 -0700

    Add median route and refactor mean route

--- app.js
+++ app.js
@@ -6,22 +6,48 @@ const app = express();
 app.use(express.json());
 
 app.get('/mean', function(req, res, next) {
+  const operation = 'mean';
   try {
-    const operation = 'mean';
     if (!req.query.nums) {
       throw new ExpressError('Nums required', 400);
     }
-    const nums = req.query.nums.split(',');
-    const value = nums.reduce((acc, n, i) => {
+    const nums = req.query.nums.split(',').map((n) => {
       if (!parseInt(n)) {
         throw new ExpressError(`${n} is not a number`, 400)
       };
+      return parseInt(n);
+    });
+    const value = nums.reduce((acc, n, i) => {
       if (i === nums.length - 1) {
         acc += parseInt(n);
         return acc / nums.length;
       }
       return acc += parseInt(n);
     }, 0)
+    return res.json({operation, value});
+  } catch(e) {
+    return next(e);
+  }
+});
+
+app.get('/median', function(req, res, next) {
+  const operation = 'median';
+  const median = (arr) => {
+    const mid = Math.floor(arr.length / 2);
+    const nums = [...arr].sort((a, b) => a - b);
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2;
+  };
+  try {
+    if (!req.query.nums) {
+      throw new ExpressError('Nums required', 400);
+    }
+    const nums = req.query.nums.split(',').map((n) => {
+      if (!parseInt(n)) {
+        throw new ExpressError(`${n} is not a number`, 400)
+      };
+      return parseInt(n);
+    });
+    const value = median(nums);
     return res.json({operation, value});
   } catch(e) {
     return next(e);
