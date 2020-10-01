67a6c3b9178fd083b293ca728adac24cd154ec9d
heyimmatt <mrmcfarland@gmail.com>
2020-09-30 23:02:54 -0700

    Add mode route

--- app.js
+++ app.js
@@ -54,6 +54,46 @@ app.get('/median', function(req, res, next) {
   }
 });
 
+app.get('/mode', function(req, res, next) {
+  const operation = 'mode';
+  function createFrequencyCounter(arr) {
+    return arr.reduce(function(acc, next) {
+      acc[next] = (acc[next] || 0) + 1;
+      return acc;
+    }, {});
+  }
+  function findMode(arr) {
+    let freqCounter = createFrequencyCounter(arr);
+  
+    let count = 0;
+    let mostFrequent;
+  
+    for (let key in freqCounter) {
+      if (freqCounter[key] > count) {
+        mostFrequent = key;
+        count = freqCounter[key];
+      }
+    }
+  
+    return +mostFrequent;
+  }
+  try {
+    if (!req.query.nums) {
+      throw new ExpressError('Nums required', 400);
+    }
+    const nums = req.query.nums.split(',').map((n) => {
+      if (!parseInt(n)) {
+        throw new ExpressError(`${n} is not a number`, 400)
+      };
+      return parseInt(n);
+    });
+    const value = findMode(nums);
+    return res.json({operation, value});
+  } catch(e) {
+    return next(e);
+  }
+});
+
 app.use(function(err, req, res, next) {
   // the default status is 500 Internal Server Error
   let status = err.status || 500;
